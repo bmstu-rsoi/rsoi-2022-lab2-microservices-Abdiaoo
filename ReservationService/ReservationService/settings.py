8a1adedaece4efddd81e1d39f7c9bf9face5bcb4
Abdiaoo <abdiooa45@gmail.com>
2022-11-05 19:57:00 +0300

    postgres setup

--- ReservationService/ReservationService/settings.py
+++ ReservationService/ReservationService/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'ReservationService.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'postgres',
+        'USER':'postgres',
+        'HOST':'postgres',
+        'PORT':5432
     }
 }
 
