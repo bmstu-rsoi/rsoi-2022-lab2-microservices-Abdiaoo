44705fbdde67997cfd4e0d5a65d7f83e2e29937c
Abdiaoo <abdiooa45@gmail.com>
2022-11-09 17:48:04 +0300

    databases setup

--- LoyaltyService/LoyaltyService/settings.py
+++ LoyaltyService/LoyaltyService/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'LoyaltyService.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'loyalties',
+        'USER':'program',
+        'PASSWORD':'test',
+        'HOST':'postgres',
+        'PORT': 5432
     }
 }
 
--- PaymentService/PaymentService/settings.py
+++ PaymentService/PaymentService/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'PaymentService.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'payments',
+        'USER':'program',
+        'PASSWORD':'test',
+        'HOST':'postgres',
+        'PORT': 5432
     }
 }
 
