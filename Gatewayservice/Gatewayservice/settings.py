2a8598d8e86f1b13ca0ea789abfa7ed38c503bc3
Abdi <112772257+Abdiaoo@users.noreply.github.com>
2022-11-18 18:27:56 +0300

    Update settings.py

--- Gatewayservice/Gatewayservice/settings.py
+++ Gatewayservice/Gatewayservice/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER':'postgres',
         'PASSWORD':'postgres',
-        'HOST':'postgres',
+        'HOST':'postgres-dbs',
         'PORT': 5432
     }
 }
