9a426585ead81690827d8b7c0d3337d070838505
Abdi <112772257+Abdiaoo@users.noreply.github.com>
2022-11-18 17:47:50 +0300

    Update settings.py

--- PaymentService/PaymentService/settings.py
+++ PaymentService/PaymentService/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'payments',
         'USER':'program',
         'PASSWORD':'test',
-        'HOST':'postgres-dbs',
+        'HOST':'postgres',
         'PORT': 5432
     }
 }
